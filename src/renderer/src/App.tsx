45d0dc7069196980ba6bcd1d2fe706db8d5f85cb
Juan Lucas Umali <85742113+juanlucasumali@users.noreply.github.com>
2024-12-19 22:26:02 -0800

    Change version to 0.0.0

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -10,7 +10,7 @@ function App(): JSX.Element {
       <div className="creator">Powered by electron-vite</div>
       <div className="text">
         Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript, version 0.0.1</span>
+        &nbsp;and <span className="ts">TypeScript, version 0.0.0</span>
       </div>
       <p className="tip">
         Please try pressing <code>F12</code> to open the devTool

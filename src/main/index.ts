6104f5ce60daa66ba4edbcd1261ddae57154f0c0
Juan Lucas Umali <85742113+juanlucasumali@users.noreply.github.com>
2024-12-19 22:22:54 -0800

    Set up electron-builder

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,22 +2,10 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import log from 'electron-log/main';
-import { updateElectronApp, UpdateSourceType } from  'update-electron-app'
-
-// Implement auto-updates
-updateElectronApp({
-  updateSource: {
-    type: UpdateSourceType.ElectronPublicUpdateService,
-    repo: 'juanlucasumali/demo',
-    host: 'https://update.electronjs.org'
-  },
-  updateInterval: '5 minutes',
-  logger: log
-})
+import { autoUpdater } from 'electron-updater'
+import log from 'electron-log'
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
@@ -39,8 +27,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -48,18 +34,34 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
+  })
+
+  // Logging
+  log.transports.file.level = 'info'
+  autoUpdater.logger = log
+
+  // Check for updates on startup
+  autoUpdater.checkForUpdatesAndNotify().catch(err => log.error(err))
+
+  // (Optional) Listen for update events
+  autoUpdater.on('update-available', () => {
+    log.info('Update available')
+  })
+
+  autoUpdater.on('update-not-available', () => {
+    log.info('No update available')
+  })
+
+  autoUpdater.on('update-downloaded', () => {
+    log.info('Update downloaded; will install now')
+  })
+
+  autoUpdater.on('error', (error) => {
+    log.error('Auto update error:', error)
   })
 
   // IPC test
@@ -68,20 +70,10 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
